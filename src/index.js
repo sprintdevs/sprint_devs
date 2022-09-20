45429bdd4e08a0a6eb7ee29a5db2e468ff7c9421
Muntasir Rahman <shakil_muntasir@hotmail.com>
2022-09-20 23:04:11 +0600

    So many improvements, I have lost count 😅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const btn = document.querySelector('button.mobile-menu-button')
 const menu = document.querySelector('div.mobile-menu')
+const backdrop = document.querySelector('#nav-backdrop')
 
 btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
+
+document.querySelectorAll('.mobile-menu a').forEach(function (a) {
+    a.addEventListener('click', () => {
+        setTimeout(() => {
+            menu.classList.toggle('hidden')
+        }, 150)
+    })
+})
+
+backdrop.addEventListener('click', () => {
     menu.classList.toggle('hidden')
 })
